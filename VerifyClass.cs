d16fe99f3cb8cf0507c8f162bd99b5e79695725c
gsp789 <saipraveen.gampa@gmail.com>
2015-12-19 23:37:09 -0600

    Update Errors in Question
    Typos in VerifyClass question updated

--- VerifyClass.cs
+++ VerifyClass.cs
@@ -14,7 +14,7 @@ The first line of input contains T, the number of test cases.
 Each test case consists of two lines. The first line has two space-separated integers, N (students in the class) and K (the cancelation threshold).
 The second line contains N space-separated integers (a1,a2,…,aN) describing the arrival times for each student.
 
-Note: Non-positive arrival times (ai=0) indicate the student arrived early or on time; positive arrival times (ai>0) indicate the student arrived ai minutes late.
+Note: Non-positive arrival times (ai<=0) indicate the student arrived early or on time; positive arrival times (ai>0) indicate the student arrived ai minutes late.
 
 Output Format
 
@@ -25,7 +25,7 @@ Constraints
     1=T=10
     1=N=1000
     1=K=N
-    -100=ai=100,where i?[1,N]
+    -100<=ai<=100,where i belongs to [1,N]
 
 Note
 If a student arrives exactly on time (ai=0), the student is considered to have entered before the class started.
